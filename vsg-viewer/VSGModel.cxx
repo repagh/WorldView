e955680fe3d446181c83e3ab4c64efefb69eaa25
René van Paassen <rene.vanpaassen@gmail.com>
2023-01-26 22:32:49 +0100

    wip model

---
+++ vsg-viewer/VSGModel.cxx
@@ -0,0 +1,25 @@
+/* ------------------------------------------------------------------   */
+/*      item            : VSGModel.cxx
+        made by         : Rene' van Paassen
+        date            : 230126
+        category        : body file
+        description     :
+        changes         : 230126 first version
+        language        : C++
+        copyright       : (c) 23 TUDelft-AE-C&S
+*/
+
+#include "VSGModel.hxx"
+
+
+VSGModel::VSGModel()
+{
+
+}
+
+
+VSGModel::~VSGModel()
+{
+
+}
+
---
+++ vsg-viewer/VSGModel.hxx
@@ -0,0 +1,26 @@
+/* ------------------------------------------------------------------   */
+/*      item            : VSGModel.hxx
+        made by         : Rene van Paassen
+        date            : 230126
+        category        : header file
+        description     :
+        changes         : 230126 first version
+        language        : C++
+        copyright       : (c) 23 TUDelft-AE-C&S
+*/
+
+#pragma once
+
+#include "VSGObject.hxx"
+
+class VSGModel: public VSGObject
+{
+  
+public:
+  /** Constructor */
+  VSGModel();
+
+  /** Destructor */
+  ~VSGModel();
+};
+
